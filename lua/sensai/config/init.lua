fc1002dd5c76827499b3ec60a543c9fccedb7467
Maximus Cisneros <mxcrcpu@gmail.com>
2023-06-01 13:21:43 -0700

    > Adding base plugin files

--- lua/sensai/config/init.lua
+++ lua/sensai/config/init.lua
@@ -1,22 +1,28 @@
 local fn = vim.fn
 local api = vim.api
-
-local create_directory = function(directory)
-end
-
+local os = require("os")
 local M = {
-	sensai_version = "1.0.0"
+	sensai_version = "1.0.0",
+	supported_models = {},
+	installed_models = {},
+	options = {},
 }
-
--- defaults string?
 local defaults = {
-	directory = nil,
+	models_directory = os.getenv("HOME") .. "/.config/nvim/models",
 }
 
 M.setup = function(opts)
 	options = vim.tbl_deep_extend("force", defaults, opts or {})
-	if fn.argc(-1) == 0 then
+	local models_directory_split = vim.fn.split(options.models_directory, "/")
+	if fn.isdirectory(options.models_directory) and
+		models_directory_split[#models_directory_split] ~= "models" then
+		options.models_directory = options.models_directory .. "/models"
 	end
+	if not fn.isdirectory(options.models_directory) then
+		fn.mkdir(options.models_directory)
+	end
+	M.options = options
+	print(vim.inspect(M.options))
 end
 
 return M
--- lua/sensai/init.lua
+++ lua/sensai/init.lua
@@ -3,6 +3,7 @@ local M = {}
 M.setup = function(opts)
 	-- print "starting sensai..."
 	require("sensai.config").setup(opts)
+	require("sensai.plugin").setup_commands()
 end
 
 return M
