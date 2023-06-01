7978f9bb505a60ce6cd1128779c82c0a2b99c069
Maximus Cisneros <mxcrcpu@gmail.com>
2023-06-01 14:02:29 -0700

    > Progress Check

--- lua/sensai/config/init.lua
+++ lua/sensai/config/init.lua
@@ -1,5 +1,6 @@
 local fn = vim.fn
-local api = vim.api
+local log = vim.log
+local cmd = vim.cmd
 local os = require("os")
 local M = {
 	sensai_version = "1.0.0",
@@ -11,18 +12,41 @@ local defaults = {
 	models_directory = os.getenv("HOME") .. "/.config/nvim/models",
 }
 
-M.setup = function(opts)
-	options = vim.tbl_deep_extend("force", defaults, opts or {})
-	local models_directory_split = vim.fn.split(options.models_directory, "/")
+M.check_python_connection = function()
+	local check_python = cmd[[!python --version]]
+	local check_poetry = cmd[[!poetry --version]]
+	print(check_python)
+	print(check_poetry)
+end
+
+M.setup_models_directory = function()
+	local models_directory_split = fn.split(options.models_directory, "/")
+	if not fn.isdirectory(options.models_directory) then
+		vim.notify(
+			"sensai.nvim: setup: models_directory is not a valid directory",
+			log.levels.ERROR,
+			{ title = "sensai.nvim" }
+		)
+		return
+	end
 	if fn.isdirectory(options.models_directory) and
-		models_directory_split[#models_directory_split] ~= "models" then
+	    models_directory_split[#models_directory_split] ~= "models" then
 		options.models_directory = options.models_directory .. "/models"
 	end
 	if not fn.isdirectory(options.models_directory) then
 		fn.mkdir(options.models_directory)
 	end
 	M.options = options
-	print(vim.inspect(M.options))
+end
+
+M.check_installed_models = function()
+end
+
+M.setup = function(opts)
+	options = vim.tbl_deep_extend("force", defaults, opts or {})
+	M.check_python_connection()
+	M.setup_models_directory()
+	M.check_installed_models()
 end
 
 return M
--- lua/sensai/init.lua
+++ lua/sensai/init.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 M.setup = function(opts)
-	-- print "starting sensai..."
 	require("sensai.config").setup(opts)
 	require("sensai.plugin").setup_commands()
 end
